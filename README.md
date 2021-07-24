00f6cc798e233501b86d0d56b42317ec0fc0cde4
Jason Vu <jvu@scu.edu>
2021-07-24 12:10:51 -0700

    last minute updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# tiktok-scraping-with-python
-
-<p align="center">
-    <img src="https://www.xbyte.io/images/services/tiktok/Tiktok-Data-Collection-with-Tiktok-API.jpg">
-</p>
--- video_scraper.py
+++ video_scraper.py
@@ -1,10 +1,18 @@
-# for getting videos 
+# for getting videos from the first video post from TikTok's trending accounts
 
 from TikTokApi import TikTokApi
 import string
 import random
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import uuid
+ 
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
 
-verifyFp = "verify_kq02cje3_QxorNTrh_UCSw_47Yj_9fs8_VJ7XXKQ8adNb"   
+verifyFp = os.environ.get("verifyFp")
+
 did = "".join(random.choice(string.digits) for num in range(19))
 
 api = TikTokApi.get_instance(custmom_verifyFp=verifyFp, use_text_endpoints=True, custom_did=did)
@@ -13,5 +21,8 @@ tiktoks = api.trending()
  
 video_bytes = api.get_Video_By_TikTok(tiktoks[0])
 
-with open("test.mp4", "wb") as o:
+filename = str(uuid.uuid4())
+with open("./videos/" + filename + "_test.mp4", "wb") as o:
     o.write(video_bytes)
+
+print("Success!")
